ed8822d8abecab6eb14ec75f9c0cb49ecdc308cf
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-07-26 12:10:42 +0700

    feat: 🎉 readme.md

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,6 +1,2 @@
-// Explicitly re-export to avoid conflicts during the build process
-// Reference: https://github.com/alan2207/bulletproof-react/issues/124
-// The conflict that occurred: "contains conflicting star exports for the name '$$ACTION_0' with the previous requested module"
-
 export { POSTAuth } from "./auth";
 export { DELETEExample, GETExample, POSTExample, PUTExample } from "./example";
