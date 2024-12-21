909716e844764dad4066c49e4ab6fba966c7d08c
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-12-21 16:29:14 +0700

    chore: ⚙️ update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
-  darkMode: ["selector", '[data-theme="dark"]'],
+  darkMode: ["selector", '[data-theme="light"]'],
   plugins: [],
   theme: {
     extend: {},
