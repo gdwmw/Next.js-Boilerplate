8e354f02f8d764fbc44617bd347efecd84516670
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-02-01 13:33:41 +0700

    chore: ⚙️ update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
 
+import plugin from "tailwindcss/plugin";
+
 const config: Config = {
   content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
   darkMode: ["selector", '[data-theme="dark"]'],
-  plugins: [],
+  plugins: [
+    plugin(({ matchVariant }) => {
+      matchVariant("h-max", (value) => `@media (max-height: ${value})`);
+      matchVariant("h-min", (value) => `@media (min-height: ${value})`);
+    }),
+  ],
   theme: {
     extend: {},
   },
