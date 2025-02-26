gdwmw/HASTINULC

feat: 🎉 next.js boilerplate by gdwmw

src/components/interfaces/example/C/elements/error-message/index.tsx
@@ -1,0 +1,11 @@
+import { FC, ReactElement } from "react";
+
+interface I {
+  errorMessage: string;
+}
+
+export const ErrorMessage: FC<I> = (props): ReactElement => (
+  <span className="ml-1 block text-xs font-semibold text-red-600" data-testid="example-error-message">
+    {props.errorMessage}
+  </span>
+);

gdwmw/Next.js-Boilerplate

src/components/interfaces/example/C/elements/error-message/index.tsx
@@ -1,0 +1,11 @@
+import { FC, ReactElement } from "react";
+
+interface I {
+  errorMessage: string;
+}
+
+export const ExampleErrorMessage: FC<I> = (props): ReactElement => (
+  <span className="ml-1 block text-xs font-semibold text-red-600" data-testid="example-error-message">
+    {props.errorMessage}
+  </span>
+);