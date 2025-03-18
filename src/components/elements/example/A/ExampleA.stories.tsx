gdwmw/HASTINULC

feat: 🎉 next.js boilerplate by gdwmw

src/components/interfaces/example/A/stories/ExampleA.stories.tsx
@@ -1,0 +1,80 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { fn } from "@storybook/test";
+import { FaCrown } from "react-icons/fa";
+
+import Layout from "./Layout";
+
+const meta: Meta<typeof Layout> = {
+  args: { onClick: fn(() => alert("Clicked!")) },
+  argTypes: {
+    color: {
+      control: { type: "radio" },
+      options: ["rose", "emerald"],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    size: {
+      control: { type: "radio" },
+      options: ["sm", "md", "lg"],
+    },
+    variant: {
+      control: { type: "radio" },
+      options: ["solid", "outline", "ghost"],
+    },
+  },
+  component: Layout,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  title: "Components/Interfaces/Example/A",
+};
+
+export default meta;
+type Story = StoryObj<typeof Layout>;
+
+/* eslint-disable perfectionist/sort-objects */
+export const Solid: Story = {
+  args: {
+    variant: "solid",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown data-testid="testing-icon" />
+        Example
+      </>
+    ),
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown data-testid="testing-icon" />
+        Example
+      </>
+    ),
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown data-testid="testing-icon" />
+        Example
+      </>
+    ),
+  },
+};
+/* eslint-enable perfectionist/sort-objects */

src/components/interfaces/example/C/stories/ExampleC.stories.tsx
@@ -1,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { FaEye } from "react-icons/fa";
+
+import Layout from "./Layout";
+
+const meta: Meta<typeof Layout> = {
+  args: {},
+  argTypes: {
+    color: {
+      control: { type: "radio" },
+      options: ["rose", "emerald"],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    errorMessage: {
+      control: { type: "text" },
+    },
+    label: {
+      control: { type: "text" },
+    },
+    type: {
+      control: { type: "text" },
+    },
+  },
+  component: Layout,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  title: "Components/Interfaces/Example/C",
+};
+
+export default meta;
+type Story = StoryObj<typeof Layout>;
+
+/* eslint-disable perfectionist/sort-objects */
+export const Input: Story = {
+  args: {
+    componentType: "input",
+    color: "rose",
+    label: "Example Input",
+    type: "text",
+    icon: <FaEye />,
+  },
+};
+
+export const Select: Story = {
+  args: {
+    componentType: "select",
+    color: "rose",
+    label: "Example Select",
+  },
+};
+
+export const TextArea: Story = {
+  args: {
+    componentType: "textarea",
+    color: "rose",
+    label: "Example TextArea",
+  },
+};
+/* eslint-enable perfectionist/sort-objects */

gdwmw/Next.js-Boilerplate

src/components/elements/example/A/ExampleA.stories.tsx
@@ -1,0 +1,80 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { fn } from "@storybook/test";
+import { FaCrown } from "react-icons/fa";
+
+import { ExampleA } from ".";
+
+const meta: Meta<typeof ExampleA> = {
+  args: { onClick: fn(() => alert("Clicked!")) },
+  argTypes: {
+    color: {
+      control: { type: "radio" },
+      options: ["rose", "emerald"],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    size: {
+      control: { type: "radio" },
+      options: ["sm", "md", "lg"],
+    },
+    variant: {
+      control: { type: "radio" },
+      options: ["solid", "outline", "ghost"],
+    },
+  },
+  component: ExampleA,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  title: "Components/Interfaces/Example/A",
+};
+
+export default meta;
+type Story = StoryObj<typeof ExampleA>;
+
+/* eslint-disable perfectionist/sort-objects */
+export const Solid: Story = {
+  args: {
+    variant: "solid",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown />
+        Example
+      </>
+    ),
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown />
+        Example
+      </>
+    ),
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    color: "rose",
+    size: "sm",
+    children: (
+      <>
+        <FaCrown />
+        Example
+      </>
+    ),
+  },
+};
+/* eslint-enable perfectionist/sort-objects */

src/components/elements/example/C/stories/ExampleC.stories.tsx
@@ -1,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { fn } from "@storybook/test";
+import { FaEye } from "react-icons/fa";
+
+import StoriesLayout from ".";
+
+const meta: Meta<typeof StoriesLayout> = {
+  args: { iconOnClick: fn(() => alert("Clicked!")) },
+  argTypes: {
+    color: {
+      control: { type: "radio" },
+      options: ["rose", "emerald"],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    errorMessage: {
+      control: { type: "text" },
+    },
+    label: {
+      control: { type: "text" },
+    },
+    type: {
+      control: { type: "text" },
+    },
+  },
+  component: StoriesLayout,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  title: "Components/Interfaces/Example/C",
+};
+
+export default meta;
+type Story = StoryObj<typeof StoriesLayout>;
+
+/* eslint-disable perfectionist/sort-objects */
+export const Input: Story = {
+  args: {
+    componentType: "input",
+    color: "rose",
+    label: "Example Input",
+    type: "text",
+    icon: <FaEye />,
+  },
+};
+
+export const Select: Story = {
+  args: {
+    componentType: "select",
+    color: "rose",
+    label: "Example Select",
+  },
+};
+
+export const TextArea: Story = {
+  args: {
+    componentType: "textarea",
+    color: "rose",
+    label: "Example TextArea",
+  },
+};
+/* eslint-enable perfectionist/sort-objects */