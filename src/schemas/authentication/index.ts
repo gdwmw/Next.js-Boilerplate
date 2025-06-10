de57101e8ba139003974433c36d33f5358b42bf0
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-06-10 15:42:24 +0700

    refactor: 💡 schemas

--- src/schemas/authentication/index.ts
+++ src/schemas/authentication/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { schemaErrorMessage } from "../schema-error-message";
+import { schemaErrorMessage as errorMessage } from "../schema-error-message";
 
 // ----------------------------
 
@@ -8,9 +8,9 @@ export const LoginSchema = (label: string) =>
   z.object({
     identifier:
       label === "Email"
-        ? z.string().email({ message: schemaErrorMessage.string.email(label) })
-        : z.string().min(1, { message: schemaErrorMessage.string.required(label) }),
-    password: z.string().min(1, { message: schemaErrorMessage.string.required("Password") }),
+        ? z.string().email({ message: errorMessage.string.email(label) })
+        : z.string().min(1, { message: errorMessage.string.required(label) }),
+    password: z.string().min(1, { message: errorMessage.string.required("Password") }),
   });
 
 export type TLoginSchema = z.infer<ReturnType<typeof LoginSchema>>;
@@ -18,17 +18,17 @@ export type TLoginSchema = z.infer<ReturnType<typeof LoginSchema>>;
 // ----------------------------
 
 export const RegisterSchema = z.object({
-  confirmPassword: z.string().min(1, { message: schemaErrorMessage.string.required("Confirm Password") }),
-  email: z.string().email({ message: schemaErrorMessage.string.email("Email") }),
-  name: z.string().min(3, { message: schemaErrorMessage.string.min("Name", 3) }),
+  confirmPassword: z.string().min(1, { message: errorMessage.string.required("Confirm Password") }),
+  email: z.string().email({ message: errorMessage.string.email("Email") }),
+  name: z.string().min(3, { message: errorMessage.string.min("Name", 3) }),
   password: z
     .string()
-    .min(8, { message: schemaErrorMessage.string.min("Password", 8) })
+    .min(8, { message: errorMessage.string.min("Password", 8) })
     .regex(/^(?=.*[A-Z])/, { message: "Password must have at least 1 uppercase letter" })
     .regex(/^(?=.*\d)/, { message: "Password must have at least 1 number" })
     .regex(/^(?=.*[!@#$%^&*])/, { message: "Password must have at least 1 symbol (!@#$%^&*)" }),
-  phoneNumber: z.string().min(10, { message: schemaErrorMessage.string.min("Phone", 10) }),
-  username: z.string().min(4, { message: schemaErrorMessage.string.min("Username", 4) }),
+  phoneNumber: z.string().min(10, { message: errorMessage.string.min("Phone", 10) }),
+  username: z.string().min(4, { message: errorMessage.string.min("Username", 4) }),
 });
 
 export type TRegisterSchema = z.infer<typeof RegisterSchema>;
--- src/schemas/example/index.ts
+++ src/schemas/example/index.ts
@@ -2,33 +2,33 @@ import { z } from "zod";
 
 import { EXAMPLE_PACKAGES_DATA } from "@/src/libs/constants";
 
-import { schemaErrorMessage } from "../schema-error-message";
+import { schemaErrorMessage as errorMessage } from "../schema-error-message";
 
 // ----------------------------
 
 export const ExampleBookingSchema = z.object({
-  date: z.string().min(1, { message: schemaErrorMessage.string.required("Date") }),
-  email: z.string().email({ message: schemaErrorMessage.string.email("Email") }),
+  date: z.string().min(1, { message: errorMessage.string.required("Date") }),
+  email: z.string().email({ message: errorMessage.string.email("Email") }),
   googleMapsLink: z
     .string()
-    .url({ message: schemaErrorMessage.string.url("Google Maps Link") })
+    .url({ message: errorMessage.string.url("Google Maps Link") })
     .refine((url) => url.includes("https://maps.app.goo.gl/"), {
-      message: schemaErrorMessage.string.url("Google Maps Link"),
+      message: errorMessage.string.url("Google Maps Link"),
     }),
-  name: z.string().min(3, { message: schemaErrorMessage.string.min("Name", 3) }),
+  name: z.string().min(3, { message: errorMessage.string.min("Name", 3) }),
   package: z.enum(EXAMPLE_PACKAGES_DATA.map((dt) => dt.title) as [string, ...string[]], {
-    errorMap: () => ({ message: schemaErrorMessage.string.enum("Package") }),
+    errorMap: () => ({ message: errorMessage.string.enum("Package") }),
   }),
-  phoneNumber: z.string().min(10, { message: schemaErrorMessage.string.min("Phone Number", 10) }),
+  phoneNumber: z.string().min(10, { message: errorMessage.string.min("Phone Number", 10) }),
   time: z
     .array(z.string())
-    .min(1, { message: schemaErrorMessage.string.enum("Time") })
+    .min(1, { message: errorMessage.string.enum("Time") })
     .or(z.literal(false).transform(() => []))
     .superRefine((val, ctx) => {
       if (Array.isArray(val) && val.length === 0) {
         ctx.addIssue({
           code: z.ZodIssueCode.custom,
-          message: schemaErrorMessage.string.enum("Time"),
+          message: errorMessage.string.enum("Time"),
         });
       }
     }),
