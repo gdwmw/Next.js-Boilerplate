gdwmw/HASTINULC

feat: 🎉 next.js boilerplate by gdwmw

src/utils/api/index.ts
@@ -1,0 +1,2 @@
+export * from "./auth";
+export * from "./example";

gdwmw/Next.js-Boilerplate

src/utils/api/index.ts
@@ -1,0 +1,5 @@
+export * from "./authentication";
+export * from "./data";
+export * from "./password";
+export * from "./upload";
+export * from "./user";